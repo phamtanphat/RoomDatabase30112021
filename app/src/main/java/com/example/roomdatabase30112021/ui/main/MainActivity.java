5cbe71ba0d2a34eaaaa4f43820dcf7d0e2d16a07
phamtanphat <phatdroid94@gmail.com>
2022-02-26 19:29:04 +0700

    update

--- app/src/main/java/com/example/roomdatabase30112021/ui/main/MainActivity.java
+++ app/src/main/java/com/example/roomdatabase30112021/ui/main/MainActivity.java
@@ -1,16 +1,42 @@
 package com.example.roomdatabase30112021.ui.main;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModel;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import com.example.roomdatabase30112021.R;
+import com.example.roomdatabase30112021.data.model.entities.WorkEntity;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+    MainViewModel mViewModel;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mViewModel = new ViewModelProvider(this, new ViewModelProvider.Factory() {
+            @NonNull
+            @Override
+            public <T extends ViewModel> T create(@NonNull Class<T> modelClass) {
+                return (T) new MainViewModel(MainActivity.this);
+            }
+        }).get(MainViewModel.class);
+
+        mViewModel.getListWorks().observe(this, new Observer<List<WorkEntity>>() {
+            @Override
+            public void onChanged(List<WorkEntity> workEntities) {
+                Log.d("BBB",workEntities.size() + "");
+            }
+        });
+
+        mViewModel.queryListWorks();
     }
 }
