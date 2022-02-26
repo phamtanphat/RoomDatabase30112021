ef42c5a9a670c59108de6479e19c43e837b7455b
phamtanphat <phatdroid94@gmail.com>
2022-02-26 18:47:23 +0700

    update

--- app/src/main/java/com/example/roomdatabase30112021/ui/main/MainActivity.java
+++ app/src/main/java/com/example/roomdatabase30112021/ui/main/MainActivity.java
@@ -1,8 +1,10 @@
-package com.example.roomdatabase30112021;
+package com.example.roomdatabase30112021.ui.main;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.example.roomdatabase30112021.R;
 
 public class MainActivity extends AppCompatActivity {
 
